63af37644541bccb0399f1decc267c0fe64d7858
megukanipan <megukanipan@yahoo.co.jp>
2014-11-22 19:33:38 +0900

    ぢだんださん

--- src/com/example/didanda/MainActivity.java
+++ src/com/example/didanda/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.didanda;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+	ImageView didandaView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		didandaView=(ImageView)findViewById(R.id.didandaView);
+	}
+	public void rightButton(View v){
+		didandaView.setImageResource(R.drawable.right_didanda);
+		
+	}
+	public void leftButton(View v){
+		didandaView.setImageResource(R.drawable.left_didanda);
+		
 	}
 
 	@Override
