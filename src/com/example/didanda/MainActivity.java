8ff1e8a975a0578567f675d7f7afbb2cd7506b44
megukanipan <megukanipan@yahoo.co.jp>
2014-11-22 20:35:35 +0900

    countup機能つけた

--- src/com/example/didanda/MainActivity.java
+++ src/com/example/didanda/MainActivity.java
@@ -1,27 +1,58 @@
 package com.example.didanda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.format.Time;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	ImageView didandaView;
-	
+	int point, five_roop;
+	TextView scoreText;
+	long timeList[];
+	long timeDifference[];
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		didandaView=(ImageView)findViewById(R.id.didandaView);
+		didandaView = (ImageView) findViewById(R.id.didandaView);
+		scoreText = (TextView) findViewById(R.id.scoreText);
 	}
-	public void rightButton(View v){
+
+	public void rightButton(View v) {
 		didandaView.setImageResource(R.drawable.right_didanda);
-		
+		countUp();
+
 	}
-	public void leftButton(View v){
+
+	public void leftButton(View v) {
 		didandaView.setImageResource(R.drawable.left_didanda);
-		
+		countUp();
+
+	}
+
+	public void countUp() {
+		point++;
+		scoreText.setText("" + point * 100);
+		five_roop++;
+		timeList[point] = System.currentTimeMillis();
+		if (point <= 1) {
+			timeDifference[five_roop] = timeList[point] - timeList[point - 1];
+		}
+
+		if (five_roop <= 4) {
+			five_roop = 0;
+		}
+	}
+	public void gameOver() {
+		//げーむおーばーになった時の処理。
 	}
 
 	@Override
